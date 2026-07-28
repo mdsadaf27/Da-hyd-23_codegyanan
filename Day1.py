791fa3ab71a6d86edc8b9dc67b65cc572d22f06d
mdsadaf27 <mdsadaf979@gmail.com>
2026-07-28 14:59:19 +0530

    Tokens-day1

---
+++ Day1.py
@@ -0,0 +1,30 @@
+print("hi")
+
+"""
+hi my is sadaf im a graduate
+
+"""
+name='afreen'
+print(name)
+email_id="mdafreen979@gmail.com"
+print(email_id)
+@num="ASDF"
+print("@num")
+ """"
+#multiassifnment
+name,location,adress='afreen','hyd',100
+print('name')
+print('location')
+print('adress')
+
+
+
+
+
+
+
+
+
+
+
+
